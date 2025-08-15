3fe41fb57570d9915c081fcd71e197406cdb81d7
VishwaS021 <vk2567243@gmail.com>
2025-08-15 18:32:03 +0530

    Add files via upload

---
+++ Task_Automation.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+source_folder = r"C:\Users\vk256"
+destination_folder = r"C:\Users\vk256\JPG images" 
+
+if not os.path.exists(destination_folder):
+    os.makedirs(destination_folder)
+
+moved_count = 0
+for file_name in os.listdir(source_folder):
+    if file_name.lower().endswith(".jpg"):
+        source_path = os.path.join(source_folder, file_name)
+        destination_path = os.path.join(destination_folder, file_name)
+        shutil.move(source_path, destination_path)
+        moved_count += 1
+
+print(f" {moved_count} .jpg file(s) moved to {destination_folder}")
